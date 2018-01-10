fbaceb8e00f2404a79d2ece9b7019f6bf422ff8a
Yomaira Escano <yjescano@gmail.com>
2018-01-10 21:19:19 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -14,7 +14,7 @@ class Application
     elsif req.path.match(/search/)
       search_term = req.params["q"]
       resp.write handle_search(search_term)
-    
+
       # if @@cart.empty?
       #   resp.write "Your cart is empty"
 
